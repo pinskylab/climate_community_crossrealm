fb613d6daf731ff4b4726f3c89e8172527359d48
Malin Pinsky <malin.pinsky@gmail.com>
2020-02-12 21:36:25 +0100

    Fix primary prod script to use rarefyID

--- code/assemble_pp.R
+++ code/assemble_pp.R
@@ -7,7 +7,7 @@ load('data/biotime_blowes/bt.Rdata')
 bt <- data.table(bt_malin); rm(bt_malin)
 
 # trim to locations
-btterra <- bt[!duplicated(STUDY_ID), ][REALM %in% c('Terrestrial', 'Freshwater'), .(STUDY_ID, lon = rarefyID_x, lat = rarefyID_y)]
-btaqua <- bt[!duplicated(STUDY_ID), ][REALM == 'Marine', .(STUDY_ID, lon = rarefyID_x, lat = rarefyID_y)]
+btterra <- bt[!duplicated(rarefyID), ][REALM %in% c('Terrestrial', 'Freshwater'), .(rarefyID, lon = rarefyID_x, lat = rarefyID_y)]
+btaqua <- bt[!duplicated(rarefyID), ][REALM == 'Marine', .(rarefyID, lon = rarefyID_x, lat = rarefyID_y)]
 
 ### Add in MODIS data
