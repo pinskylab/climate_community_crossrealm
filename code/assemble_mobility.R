d0a16c9738ef082b77f2a68bafa57847e73ca4b5
Malin Pinsky <malin.pinsky@gmail.com>
2020-05-12 18:30:53 +0200

    Fix bug in mobility calculation (g to kg)

--- code/assemble_mobility.R
+++ code/assemble_mobility.R
@@ -4,14 +4,30 @@
 
 require(data.table)
 
-# read in data
-mass <- fread('output/mass_byspecies.csv.gz')
-phylo <- fread('output/taxonomy.csv.gz')
+##############
+# Load data
+##############
+mass <- fread('output/mass_byspecies.csv.gz') # assembled data on mass, in g
+phylo <- fread('output/taxonomy.csv.gz') # phylogeny info
 
-# merge in taxonomy
-phylo[, original_name := gsub(' ', '_', original_name)]
-phylo[, ':='(gateway.masses = NULL, V1 = NULL, X = NULL, NA. = NULL)]
+# BioTime change (for taxa_mod) and species
+load('data/biotime_blowes/bt_malin.Rdata')
+bt <- data.table(bt_malin); rm(bt_malin) # rename to bt
+load('data/biotime_blowes/bt_grid_spp_list.Rdata') # loads bt_grid_spp_list. this has some studies not in bt.
+btspp <- data.table(bt_grid_spp_list); rm(bt_grid_spp_list) # rename to btspp
+btspp <- merge(btspp, bt[!duplicated(rarefyID), .(rarefyID, taxa_mod)], by = 'rarefyID') # add taxa_mod to spp list and trims to spp in bt
+
+#################
+# process data
+#################
+
+# merge taxonomy and mass
+phylo[, original_name := gsub(' ', '_', original_name)] # add _ back to name
+phylo[, ':='(gateway.masses = NULL, V1 = NULL, X = NULL, NA. = NULL)] # remove extraneous columns
 mob <- merge(mass[, .(original_name = Species, rarefyID, REALM, STUDY_ID, taxa_mod, mass, mass_source)], phylo, by = 'original_name', all.x = TRUE)
+mob <- merge(btspp[, .(rarefyID, original_name = Species, REALM, STUDY_ID, taxa_mod)], 
+             mob[, .(original_name, rarefyID, mass, mass_source, kingdom, phylum, class, order, family, genus, species)], 
+             all.x = TRUE, by = c("rarefyID", "original_name"))
 
 # make a pseudo-genus from original_name
 mob[, original_genus := vapply(strsplit(original_name, "_"), `[`, 1, FUN.VALUE=character(1))]
@@ -61,33 +77,34 @@ mob[order %in% c('Sessilia', 'Anthoathecata', 'Leptothecata'), mode := 'stationa
 
 
 # examine what is missing
-mob[is.na(mode) & !duplicated(species), table(phylum)]
+mob[is.na(mode) & !duplicated(species), table(phylum)] # nothing missing that has a scientific name match
 mob[is.na(mode) & !duplicated(original_name), table(mass_source)]
 
 
 # calculate max speed (km/hr) from allometry
 # Table S4 in Hirt et al. 2017 Nature E&E
+mob[, mass_kg := mass * 1000] # convert to kg from g
 mob[, speed := NA_real_]
-mob[mode == 'flying', speed := 142.8 * mass^0.24 * (1 - exp(-2.4 * mass^(-0.72)))]
-mob[mode == 'running', speed := 25.5 * mass^0.26 * (1 - exp(-22 * mass^(-0.6)))]
-mob[mode == 'crawling', speed := 0.1] # below the slowest runners
-mob[mode == 'swimming', speed := 11.2 * mass^0.36 * (1 - exp(-19.5 * mass^(-0.56)))]
+mob[mode == 'flying', speed := 142.8 * mass_kg^0.24 * (1 - exp(-2.4 * mass_kg^(-0.72)))]
+mob[mode == 'running', speed := 25.5 * mass_kg^0.26 * (1 - exp(-22 * mass_kg^(-0.6)))]
+mob[mode == 'crawling', speed := 0.1] # set crawlers to something below the slowest runners
+mob[mode == 'swimming', speed := 11.2 * mass_kg^0.36 * (1 - exp(-19.5 * mass_kg^(-0.56)))]
 mob[mode == 'stationary', speed := 0]
 
 
 # check coverage
 mob[, .(n = length(unique(original_name)), val = sum(!is.na(speed))), by = rarefyID][, hist(val/n)] # most have >50% of species represented!
-mob[, .(n = length(unique(original_name)), val = sum(!is.na(speed))), by = rarefyID][(val/n) < 0.5, ] # 1278 rarefyID with <50%
+mob[, .(n = length(unique(original_name)), val = sum(!is.na(speed))), by = rarefyID][(val/n) < 0.5, ] # 5018 rarefyID with <50%
 
 
 
 # make a list of priority studies for further mobility finding
-# less thatn 50% species with data, at least 5 species in the study
-priorities <- mob[, .(n = length(unique(original_name)), val = sum(!is.na(speed))), by = 
+# have mass, but less than 50% species with mobility, at least 5 species in the study with mass
+priorities <- mob[!is.na(mass), .(n = length(unique(original_name)), val = sum(!is.na(speed))), by = 
                          .(rarefyID, STUDY_ID, taxa_mod)][, .(n = sum(n), val = sum(val)), by = .(STUDY_ID, taxa_mod)][(val/n) < 0.5 & n > 4, STUDY_ID]
 length(priorities) # 13 studies
 priorities.out <- mob[STUDY_ID %in% priorities & is.na(speed), .(original_name, original_genus, REALM, taxa_mod)][!duplicated(cbind(original_name, REALM, taxa_mod)), .(REALM, taxa_mod, original_name, original_genus)] # 552 species
-nrow(priorities.out) # 289 taxa
+nrow(priorities.out) # 1491 taxa
 
 priorities.out[, .(n = length(original_name)), by = original_genus][order(n, decreasing = TRUE)]
 
@@ -105,7 +122,7 @@ priorities.out[original_genus == 'Isopoda',]
 # output speed values
 mob.out <- mob[!is.na(speed), .(original_name, rarefyID, REALM, STUDY_ID, taxa_mod, speed, mode)]
 nrow(mob)
-nrow(mob.out) # 748738
+nrow(mob.out) # 748734
 
 write.csv(mob.out, gzfile('output/speed_byspecies.csv.gz'), row.names = FALSE)
 
@@ -117,10 +134,18 @@ mob.sum <- mob[, .(speed_mean = mean(speed, na.rm = TRUE), speed_sd = sd(speed, 
                      by = .(STUDY_ID, rarefyID, REALM, taxa_mod)]
 mob.sum[is.nan(speed_mean), speed_mean := NA_real_]
 mob.sum[is.nan(speed_geomean), speed_geomean := NA_real_]
-nrow(mob.sum) # 53092
+nrow(mob.sum) # 53467
 setkey(mob.sum, STUDY_ID, rarefyID)
 mob.sum
 
 write.csv(mob.sum, gzfile('output/speed_byrarefyID.csv.gz'))
+
+
+# output list of priority species
+setkey(priorities.out, REALM, taxa_mod, original_genus, original_name)
+nrow(priorities.out) # 1491
+priorities.out
+
+write.csv(priorities.out, gzfile('output/speed_prioritymissing.csv.gz'))
 
 
