6f611bc1c5b8c928de4628909a012d184c1d5174
Malin Pinsky <malin.pinsky@gmail.com>
2020-05-18 12:44:12 +0200

    Use GBIF names to find more TRY plant masses

--- code/extract_try.R
+++ code/extract_try.R
@@ -26,10 +26,18 @@ nrow(btspp) # 1034700
 btspp[, genus := tolower(vapply(strsplit(Species, "_"), `[`, 1, FUN.VALUE=character(1)))] # genus name in lower case
 btspp[, speciesnm := tolower(vapply(strsplit(Species, "_"), `[`, 2, FUN.VALUE=character(1)))] # genus name in lower case
 
+# load gbif species names
+phylo = fread('output/taxonomy.csv.gz', drop = 1)
+
+# merge spp with gbif names
+btspp2 <- merge(btspp, phylo[, .(class_gbif = class, genus_gbif = tolower(genus), Species_gbif = tolower(species), Species = gsub(' ', '_', original_name))], all.x = TRUE, by = 'Species')
+btspp2[, speciesnm_gbif := tolower(vapply(strsplit(Species_gbif, " "), `[`, 2, FUN.VALUE=character(1)))]
+
 # load TRY traits plants
 trypl <- fread('dataDL/try/8916.txt', encoding = 'Latin-1')
 
-# clean species names
+
+# clean species names from TRY traits
 trypl[which(stri_enc_mark(AccSpeciesName)!='ASCII'), AccSpeciesName] # examine the non-ASCII ones
 trypl[which(stri_enc_mark(AccSpeciesName)!='ASCII'), stri_enc_mark(AccSpeciesName)] # "latin1" encoding
 trypl[which(stri_enc_mark(AccSpeciesName)!='ASCII'), stri_trans_general(AccSpeciesName, "Latin-ASCII")]
@@ -43,10 +51,11 @@ trypl[which(stri_enc_mark(AccSpeciesName)!='ASCII'), AccSpeciesName_clean := str
 trypl[which(stri_enc_mark(AccSpeciesName)!='ASCII'), AccSpeciesName_clean] # examine the non-ASCII ones
 trypl[which(stri_enc_mark(AccSpeciesName)!='ASCII'), stri_enc_mark(AccSpeciesName_clean)] # encoding is now ASCII
 
-#########################
-# Find mass for plants
-#########################
-# examine
+
+###############################
+# Find mass for plants in TRY
+################################
+# examine (optional)
 trypl[, sort(unique(TraitName))] # "Dispersal distance", "Dispersal syndrome", "Plant biomass and allometry: Plant dry mass", "Plant biomass and allometry: Plant mass", "Plant fresh mass", "Plant lifespan (longevity)"         
 trypl[, unique(TraitName), by = TraitID]
 
@@ -93,57 +102,119 @@ trymass[!is.na(mean) & TraitID == 3451, ] # examine. 277 species for mass
 # calculate genera averages
 trymassgenu <- trymass[, .(mean = mean(mean), n = sum(n)), by = .(TraitID, TraitName, genus)] # genus-level averages
 
+#####################################
 # merge with plant traits by species
-btplants <- merge(btspp, trymass[TraitID == 700, .(genus, speciesnm, mass_dry_species = mean)], by = c("genus", "speciesnm"), all.x = TRUE)
-btplants <- merge(btplants, trymass[TraitID == 3407, .(genus, speciesnm, mass_fresh_species = mean)], by = c("genus", "speciesnm"), all.x = TRUE)
-btplants <- merge(btplants, trymass[TraitID == 3451, .(genus, speciesnm, mass_species = mean)], by = c("genus", "speciesnm"), all.x = TRUE)
+#####################################
+# merge based on biotime name
+btplants <- merge(btspp2, trymass[TraitID == 700, .(genus, speciesnm, mass_dry_species_bt = mean)], by = c("genus", "speciesnm"), all.x = TRUE)
+btplants <- merge(btplants, trymass[TraitID == 3407, .(genus, speciesnm, mass_fresh_species_bt = mean)], by = c("genus", "speciesnm"), all.x = TRUE)
+btplants <- merge(btplants, trymass[TraitID == 3451, .(genus, speciesnm, mass_species_bt = mean)], by = c("genus", "speciesnm"), all.x = TRUE)
+
+# merge based on gbif name
+btplants <- merge(btplants, trymass[TraitID == 700, .(genus_gbif = genus, speciesnm_gbif = speciesnm, mass_dry_species_gbif = mean)], by = c("genus_gbif", "speciesnm_gbif"), all.x = TRUE)
+btplants <- merge(btplants, trymass[TraitID == 3407, .(genus_gbif = genus, speciesnm_gbif = speciesnm, mass_fresh_species_gbif = mean)], by = c("genus_gbif", "speciesnm_gbif"), all.x = TRUE)
+btplants <- merge(btplants, trymass[TraitID == 3451, .(genus_gbif = genus, speciesnm_gbif = speciesnm, mass_species_gbif = mean)], by = c("genus_gbif", "speciesnm_gbif"), all.x = TRUE)
+
 
 # look at the matches
-btplants[!is.na(mass_dry_species), ]
-btplants[!is.na(mass_fresh_species), ]
-btplants[!is.na(mass_species), ]
+btplants[!is.na(mass_dry_species_bt), ]
+btplants[!is.na(mass_fresh_species_bt), ]
+btplants[!is.na(mass_species_bt), ]
+btplants[!is.na(mass_dry_species_gbif), ]
+btplants[!is.na(mass_fresh_species_gbif), ]
+btplants[!is.na(mass_species_gbif), ]
 
-btplants[!is.na(mass_dry_species), length(unique(Species))] # 71 species
-btplants[!is.na(mass_fresh_species), length(unique(Species))] # 16
-btplants[!is.na(mass_species), length(unique(Species))] # 67
+btplants[!is.na(mass_dry_species_bt), length(unique(Species))] # 71 species
+btplants[!is.na(mass_fresh_species_bt), length(unique(Species))] # 16
+btplants[!is.na(mass_species_bt), length(unique(Species))] # 67
+btplants[!is.na(mass_dry_species_gbif), length(unique(Species))] # 72 species
+btplants[!is.na(mass_fresh_species_gbif), length(unique(Species))] # 14
+btplants[!is.na(mass_species_gbif), length(unique(Species))] # 73
 
-btplants[!is.na(mass_dry_species) | !is.na(mass_fresh_species) | !is.na(mass_species), sort(unique(taxa_mod))] # some are not labeled as Benthos by BioTime
-btplants[(!is.na(mass_dry_species) | !is.na(mass_fresh_species) | !is.na(mass_species)) & taxa_mod != 'Plant', 
+btplants[!is.na(mass_dry_species_gbif) & is.na(mass_dry_species_bt), length(unique(Species))] # 4 new species from gbif
+btplants[!is.na(mass_fresh_species_gbif) & is.na(mass_fresh_species_bt), length(unique(Species))] # 0 new
+btplants[!is.na(mass_species_gbif) & is.na(mass_species_bt), length(unique(Species))] # 8 new
+
+btplants[mass_dry_species_bt != mass_dry_species_gbif, ] # 0 cases where bt and gbif do not match
+btplants[mass_fresh_species_bt != mass_fresh_species_gbif, ] # 0
+btplants[mass_species_bt != mass_species_gbif, ] # 0
+
+
+btplants[!is.na(mass_dry_species_bt) | !is.na(mass_fresh_species_bt) | !is.na(mass_species_bt) |
+           !is.na(mass_dry_species_gbif) | !is.na(mass_fresh_species_gbif) | !is.na(mass_species_gbif), sort(unique(taxa_mod))] # some are not labeled as Plant by BioTime
+btplants[(!is.na(mass_dry_species_bt) | !is.na(mass_fresh_species_bt) | !is.na(mass_species_bt) |
+            !is.na(mass_dry_species_gbif) | !is.na(mass_fresh_species_gbif) | !is.na(mass_species_gbif)) & taxa_mod != 'Plant', 
          .(Species, taxa_mod)][!duplicated(cbind(Species, taxa_mod)),] # one in Benthos. Looks correct.
 
 # check for missing
-btplants[taxa_mod == 'Plant' & (is.na(mass_dry_species) & is.na(mass_fresh_species) & is.na(mass_species)), length(unique(Species))] # 7292
-btplants[taxa_mod == 'Plant' & (is.na(mass_dry_species) & is.na(mass_fresh_species) & is.na(mass_species)), 
+btplants[taxa_mod == 'Plant' & (is.na(mass_dry_species_bt) & is.na(mass_fresh_species_bt) & is.na(mass_species_bt) &
+                                  is.na(mass_dry_species_gbif) & is.na(mass_fresh_species_gbif) & is.na(mass_species_gbif)), length(unique(Species))] # 7280
+btplants[taxa_mod == 'Plant' & (is.na(mass_dry_species_bt) & is.na(mass_fresh_species_bt) & is.na(mass_species_bt) &
+                                  is.na(mass_dry_species_gbif) & is.na(mass_fresh_species_gbif)), 
          sort(unique(Species))] # mostly species names and genus names. Some common names.
 
 
-# merge genus-values with species list
-btplants <- merge(btplants, trymassgenu[TraitID == 700, .(genus, mass_dry_genus = mean)], by = 'genus', all.x = TRUE)
-btplants <- merge(btplants, trymassgenu[TraitID == 3407, .(genus, mass_fresh_genus = mean)], by = 'genus', all.x = TRUE)
-btplants <- merge(btplants, trymassgenu[TraitID == 3451, .(genus, mass_genus = mean)], by = 'genus', all.x = TRUE)
+# merge genus-values with species list based on biotime genus
+btplants <- merge(btplants, trymassgenu[TraitID == 700, .(genus, mass_dry_genus_bt = mean)], by = 'genus', all.x = TRUE)
+btplants <- merge(btplants, trymassgenu[TraitID == 3407, .(genus, mass_fresh_genus_bt = mean)], by = 'genus', all.x = TRUE)
+btplants <- merge(btplants, trymassgenu[TraitID == 3451, .(genus, mass_genus_bt = mean)], by = 'genus', all.x = TRUE)
+
+# merge genus-values with species list based on gbif genus
+btplants <- merge(btplants, trymassgenu[TraitID == 700, .(genus_gbif = genus, mass_dry_genus_gbif = mean)], by = 'genus_gbif', all.x = TRUE)
+btplants <- merge(btplants, trymassgenu[TraitID == 3407, .(genus_gbif = genus, mass_fresh_genus_gbif = mean)], by = 'genus_gbif', all.x = TRUE)
+btplants <- merge(btplants, trymassgenu[TraitID == 3451, .(genus_gbif = genus, mass_genus_gbif = mean)], by = 'genus_gbif', all.x = TRUE)
+
 
 # examine genus values
-btplants[!is.na(mass_dry_genus), .(Species, genus, mass_dry_species, mass_dry_genus)]
-btplants[!is.na(mass_fresh_genus), .(Species, genus, mass_fresh_species, mass_fresh_genus)]
-btplants[!is.na(mass_genus), .(Species, genus, mass_species, mass_genus)]
+btplants[!is.na(mass_dry_genus_bt), .(Species, genus, mass_dry_species_bt, mass_dry_genus_bt)]
+btplants[!is.na(mass_fresh_genus_bt), .(Species, genus, mass_fresh_species_bt, mass_fresh_genus_bt)]
+btplants[!is.na(mass_genus_bt), .(Species, genus, mass_species_bt, mass_genus_bt)]
 
-# choose from species- or genus-level mass values
-btplants[, mass_dry := mass_dry_species]
-btplants[is.na(mass_dry_species), mass_dry := mass_dry_genus] # use common where genus-level value is NA
+btplants[!is.na(mass_dry_genus_gbif), .(Species, genus, mass_dry_species_gbif, mass_dry_genus_gbif)]
+btplants[!is.na(mass_fresh_genus_gbif), .(Species, genus, mass_fresh_species_gbif, mass_fresh_genus_gbif)]
+btplants[!is.na(mass_genus_gbif), .(Species, genus, mass_species_gbif, mass_genus_gbif)]
 
-btplants[, mass_fresh := mass_fresh_species]
-btplants[is.na(mass_fresh_species), mass_fresh := mass_fresh_genus] # use common where genus-level value is NA
+btplants[!is.na(mass_dry_genus_bt) & is.na(mass_dry_species_bt), .N] # genus adds 937 values
+btplants[!is.na(mass_fresh_genus_bt) & is.na(mass_fresh_species_bt), .N] # 145
+btplants[!is.na(mass_genus_bt) & is.na(mass_species_bt), .N] # 1726
 
-btplants[, mass := mass_species]
-btplants[is.na(mass_species), mass := mass_genus] # use common where genus-level value is NA
+btplants[!is.na(mass_dry_genus_gbif) & is.na(mass_dry_genus_bt), .N] # gbif adds 40 genera
+btplants[!is.na(mass_fresh_genus_gbif) & is.na(mass_fresh_genus_bt), .N] # 5
+btplants[!is.na(mass_genus_gbif) & is.na(mass_genus_bt), .N] # 76
 
-btplants[!is.na(mass_dry), length(unique(Species))] # 534 species now with data
-btplants[!is.na(mass_fresh), length(unique(Species))] # 104 species now with data
-btplants[!is.na(mass), length(unique(Species))] # 813 species now with data
+btplants[mass_dry_genus_bt != mass_dry_genus_gbif, ] # 2 cases (2 species in 1 genus) where BT and GBIF genus masses do not match
+btplants[mass_fresh_genus_bt != mass_fresh_genus_gbif, ] # 0
+btplants[mass_genus_bt != mass_genus_gbif, ] # 5
 
-btplants[mass_dry_genus != mass_dry_species & !is.na(mass_dry_genus), plot(mass_dry_genus, mass_dry_species, log='xy')] # compare species and genus. pretty well correlated
-btplants[mass_fresh_genus != mass_fresh_species & !is.na(mass_fresh_genus), plot(mass_fresh_genus, mass_fresh_species, log='xy')] # compare species and genus. maybe correlated
-btplants[mass_genus != mass_species & !is.na(mass_genus), plot(mass_genus, mass_species, log='xy')] # compare species and genus. mabye correlated
+# choose from species- or genus-level mass values and from BioTime vs. GBIF names. 
+# Prefer GBIF species, then BT species, then GBIF genus
+btplants[, mass_dry := mass_dry_species_gbif] # prefer GBIF species
+btplants[is.na(mass_dry), mass_dry := mass_dry_species_bt] # then BT species
+btplants[is.na(mass_dry), mass_dry := mass_dry_genus_gbif] # then GBIF genus
+btplants[is.na(mass_dry), mass_dry := mass_dry_genus_bt] # then BT genus
+
+btplants[, mass_fresh := mass_fresh_species_gbif]
+btplants[is.na(mass_fresh), mass_fresh := mass_fresh_species_bt]
+btplants[is.na(mass_fresh), mass_fresh := mass_fresh_genus_gbif]
+btplants[is.na(mass_fresh), mass_fresh := mass_fresh_genus_bt]
+
+btplants[, mass := mass_species_gbif]
+btplants[is.na(mass), mass := mass_species_bt]
+btplants[is.na(mass), mass := mass_genus_gbif]
+btplants[is.na(mass), mass := mass_genus_bt]
+
+btplants[!is.na(mass_dry), length(unique(Species))] # 557 species now with data
+btplants[!is.na(mass_fresh), length(unique(Species))] # 108 species now with data
+btplants[!is.na(mass), length(unique(Species))] # 868 species now with data
+
+btplants[mass_dry_genus_bt != mass_dry_species_bt & !is.na(mass_dry_genus_bt), plot(mass_dry_genus_bt, mass_dry_species_bt, log='xy')] # compare species and genus. pretty well correlated
+btplants[mass_dry_genus_gbif != mass_dry_species_gbif & !is.na(mass_dry_genus_gbif), plot(mass_dry_genus_gbif, mass_dry_species_gbif, log='xy')] # compare species and genus. pretty well correlated
+
+btplants[mass_fresh_genus_bt != mass_fresh_species_bt & !is.na(mass_fresh_genus_bt), plot(mass_fresh_genus_bt, mass_fresh_species_bt, log='xy')] # compare species and genus. maybe correlated
+btplants[mass_fresh_genus_gbif != mass_fresh_species_gbif & !is.na(mass_fresh_genus_gbif), plot(mass_fresh_genus_gbif, mass_fresh_species_gbif, log='xy')] # compare species and genus. maybe correlated
+
+btplants[mass_genus_bt != mass_species_bt & !is.na(mass_genus_bt), plot(mass_genus_bt, mass_species_bt, log='xy')] # compare species and genus. mabye correlated
+btplants[mass_genus_gbif != mass_species_gbif & !is.na(mass_genus_gbif), plot(mass_genus_gbif, mass_species_gbif, log='xy')] # compare species and genus. mabye correlated
 
 # compare among the different mass values
 btplants[!is.na(mass_dry) & !is.na(mass_fresh), plot(mass_dry, mass_fresh, log='xy')] # not well correlated
@@ -154,10 +225,11 @@ btplants[!is.na(mass_fresh) & !is.na(mass), plot(mass_fresh, mass, log='xy')] # 
 # trim to species with data
 nrow(btplants) # 1,034,700
 btplants.out <- btplants[!is.na(mass_dry) | !is.na(mass_fresh) | !is.na(mass), ]
-nrow(btplants.out) # 2742
+nrow(btplants.out) # 2855
 
 ### write out. mass in g
 write.csv(btplants.out, file = gzfile('output/mass_tryplants.csv.gz'))
+
 
 
 #########################
