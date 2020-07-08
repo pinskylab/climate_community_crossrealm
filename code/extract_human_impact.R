730dd5dd8266baae72e9dd78b16bc50c2fad550e
Malin Pinsky <malin.pinsky@gmail.com>
2020-07-08 12:20:33 +0200

    Extract Venter and Halpern human impact indices

--- code/extract_human_impact.R
+++ code/extract_human_impact.R
@@ -5,11 +5,8 @@ require(raster) # for geotiff
 require(ggplot2) # for plotting
 
 # Load human footprint (Venter and Halpern)
-# hfp <- raster("dataDL/terrestrial_footprint/HFP1993.tif") # terrestrial human footprint (Venter)
-# himp <- raster('dataDL/human_impact_marine/model.tif') # marine human impact (Halpern)
-# proj4string(himp) <- CRS('+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=km +no_defs') # since WGS 1984 Mollweide
-# extent(himp)
-#plot(himp)
+hfp <- raster("dataDL/terrestrial_footprint/HFP1993.tif") # terrestrial human footprint (Venter)
+himp <- raster('dataDL/human_impact_marine/masked_model.tif') # marine human impact (Halpern)
 
 # Load Bowler anthropogenic threats
 atc <- stack("dataDL/bowler_atcs/Figure_6.grd") # raster stack. Cumulative is all of them together
@@ -27,21 +24,18 @@ proj4string(btcoords) <- CRS('+init=epsg:4326') # set coordinate system to WGS84
 
 
 # Extract from HFP
-# bt1 <- spTransform(btcoords, proj4string(hfp)) # convert points to HFP coord system
-# hfpval <- extract(hfp, bt1)
-# bt[, hfp := hfpval]
+bt1 <- spTransform(btcoords, proj4string(hfp)) # convert points to HFP coord system
+hfpval <- extract(hfp, bt1)
+bt[, hfp := hfpval]
 
 
 # Extract from HIMP
-# Not working on western and southern hemisphere values
-# bt2 <- spTransform(btcoords, proj4string(himp)) # convert to HIMP coord system
-# extent(bt2) # doesn't match himp
-# extent(himp) 
-# 
-# himpval <- extract(himp, bt2)
-# bt[, himp := himpval]
-# 
-# bt[, summary(himp)]
+bt2 <- spTransform(btcoords, proj4string(himp)) # convert to HIMP coord system
+ 
+himpval <- extract(himp, bt2)
+bt[, himp := himpval]
+ 
+bt[, summary(himp)]
 
 
 # Extract from Bowler ATCs
@@ -51,24 +45,25 @@ bt[, atc := atcval]
 
 
 # plot maps
-# ggplot(bt, aes(rarefyID_x, rarefyID_y, color = hfp)) +
-#     geom_point(size = 0.3)
-# 
-# ggplot(bt, aes(rarefyID_x, rarefyID_y, color = himp)) +
-#     geom_point(size = 0.3)
+ggplot(bt, aes(rarefyID_x, rarefyID_y, color = hfp)) +
+    geom_point(size = 0.3)
+
+ggplot(bt, aes(rarefyID_x, rarefyID_y, color = himp)) +
+     geom_point(size = 0.3)
 
 ggplot(bt, aes(rarefyID_x, rarefyID_y, color = atc)) +
     geom_point(size = 0.3)
 
 # plot pairwise
-# ggplot(bt, aes(hfp, atc)) +
-#     geom_point(size = 0.3, alpha = 0.5)
-# 
-# ggplot(bt, aes(himp, atc)) +
-#     geom_point(size = 0.3, alpha = 0.5)
+ggplot(bt, aes(hfp, atc)) +
+    geom_point(size = 0.3, alpha = 0.5)
+
+ggplot(bt, aes(himp, atc)) +
+    geom_point(size = 0.3, alpha = 0.5)
 
 # stats
-# bt[, cor.test(hfp, atc, use = 'pairwise.complete.obs')]
+bt[, cor.test(hfp, atc, use = 'pairwise.complete.obs')]
+bt[, cor.test(himp, atc, use = 'pairwise.complete.obs')]
 
-# write out Bowler
-write.csv(bt[, .(rarefyID, atc)], file = gzfile('output/humanimpact_by_rarefyID.csv.gz'), row.names = FALSE)
+# write out
+write.csv(bt[, .(rarefyID, hfp, himp, atc)], file = gzfile('output/humanimpact_by_rarefyID.csv.gz'), row.names = FALSE)
