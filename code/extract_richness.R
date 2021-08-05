e2aa27e16c3610b08d4638089b30de80c5dc13a2
Malin Pinsky <malin.pinsky@gmail.com>
2021-08-05 11:39:24 -0400

    Update richness calcs to include species with 0 recorded abundance (eg, missing data)

--- code/extract_richness.R
+++ code/extract_richness.R
@@ -4,37 +4,48 @@ require(data.table)
 
 
 # BioTime studies
-load('data/biotime_blowes/bt_malin.Rdata') # load bt_malin, has our focal studies
-bt <- data.table(bt_malin); rm(bt_malin) # rename to bt
+load(here::here('data', 'biotime_blowes', 'all_pairs_beta.Rdata')) # load rarefied_beta_medians, which has all pairwise dissimilarities
+bt <- data.table(rarefied_beta_medians); rm(rarefied_beta_medians)
 
 # BioTime species
 load('data/biotime_blowes/bt_grid_spp_list.Rdata') # loads bt_grid_spp_list. this has species list per study, including some studies not in bt.
 btspp <- data.table(bt_grid_spp_list); rm(bt_grid_spp_list) # rename to btspp
+
+length(setdiff(bt$rarefyID, btspp$rarefyID)) # not missing any
+
 btspp <- merge(btspp, bt[!duplicated(rarefyID), .(rarefyID)], by = 'rarefyID', all.y = TRUE) # trim to focal studies
 
 # BioTime abundance
 load('data/biotime_blowes/bt_grid_spp_list_abund.Rdata') # loads abundance data
 btabund <- as.data.table(bt_grid_spp_list_abund); rm(bt_grid_spp_list_abund, bt_grid_spp_list_abund_year) # rename and delete unneeded df
+
+length(setdiff(bt$rarefyID, btabund$rarefyID)) # not missing any
+
 btspp <- merge(btspp, btabund[, .(rarefyID, Species, N_bar)], by = c('rarefyID', 'Species'), all.x = TRUE)
 
-btspp[, length(unique(Species))] # 26289 species
-nrow(btspp) #1034700
+btspp[, length(unique(Species))] # 27876 species
+nrow(btspp) #1142602
 
-# remove species with 0 abundance
-btspp <- btspp[N_bar > 0, ]
-
-btspp[, length(unique(Species))] # 23805 species
-nrow(btspp) #1010338
-
-# calculate richness
-rich <- btspp[, .(Nspp = .N), by = rarefyID]
+# calculate richness in 3 ways: total species, total species with abund > 0 or NA, or total species with abund > 0 and not NA
+rich <- btspp[, .(Nspp = .N, 
+                  NsppNAorNonZero = sum(N_bar > 0 | is.na(N_bar)),
+                  NsppNonZero = sum(N_bar > 0)), by = rarefyID]
 
 rich
 
+rich[Nspp != NsppNAorNonZero, ] # where trimming to abund >0 makes a difference. 630
+summary(rich[Nspp != NsppNAorNonZero, ])
+
+rich[Nspp != NsppNonZero, ] # where trimming to abund >0 and !NA makes a difference. 629
+summary(rich[Nspp != NsppNonZero, ])
+
 rich[, hist(Nspp)] # highly right-skewed
 rich[, summary(Nspp)] # 1 to 1427
-rich[, sum(Nspp == 1)] # 457
-rich[, sum(Nspp <= 2)] # 2697
+rich[, sum(Nspp == 1)] # 596
+rich[, sum(Nspp <= 2)] # 3255
+
+# how many missing timeseries? These had no species.
+length(setdiff(bt$rarefyID, rich$rarefyID)) # 0
 
 # write out
 write.csv(rich, file = gzfile('output/richness_by_rarefyID.csv.gz'), row.names = FALSE)
