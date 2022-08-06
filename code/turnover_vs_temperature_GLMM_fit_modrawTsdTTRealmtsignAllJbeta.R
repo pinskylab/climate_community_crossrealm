8629418a9e88568c70be91ae34bb43153c39dbfa
Malin Pinsky <malin.pinsky@gmail.com>
2022-08-06 16:45:41 -0400

    Fit core models with Jbeta and Horn.

---
+++ code/turnover_vs_temperature_GLMM_fit_modrawTsdTTRealmtsignAllJbeta.R
@@ -0,0 +1,123 @@
+#!/usr/bin/Rscript --vanilla
+
+# Script to fit glmmTMB modrawTsdTTRealmtsignAllJbeta models
+# nohup code/turnover_vs_temperature_GLMM_fit_modrawTsdTTRealmtsignAllJbeta.R XX > logs/turnover_vs_temperature_GLMMXX.Rout &
+# where XX is a model name (see options below)
+# (this works if code is executable, e.g., chmod u+x code/turnover_vs_temperature_GLMM_fit_modrawTsdTTRealmtsignAllJbeta.R)
+# (otherwise using nohup Rscript ...)
+
+# Read command line arguments ############
+
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+
+if (length(args) < 1)
+    stop("Have to specify a model to fit", call. = FALSE)
+if (length(args) > 1)
+    stop("Have to specify only 1 model to fit", call. = FALSE)
+fitmod <- args[1]
+MATCHMOD <-
+    FALSE # indicator to check if the argument matched a model name
+
+# print basic info about the job ############################
+
+print(paste('This is script turnover_vs_temperature_GLMM_fit_modrawTsdTTRealmtsignAllJbeta.R'))
+print(paste('This is process #', Sys.getpid()))
+print(Sys.time())
+
+
+
+# load libraries ############################
+
+library(data.table) # for handling large datasets
+library(glmmTMB, lib.loc = "/usr/lib64/R/library") # for ME models
+library(performance) # for R2
+
+# load data ############################
+
+# Turnover and covariates assembled by assemble_turnover_covariates.Rmd
+trendsall <- fread('output/turnover_w_covariates.csv.gz')
+
+trendsall[, tsign := as.factor(tsign)]
+
+# Models ############################
+
+## Choose dataset
+iallJbeta <-
+    trendsall[, complete.cases(
+        Jbeta.sc,
+        tempchange_abs.sc,
+        REALM,
+        tempave_metab.sc,
+        durationlog.sc,
+        mass.sc,
+        nspp.sc,
+        seas.sc,
+        microclim.sc,
+        npp.sc,
+        human_bowler.sc
+    )]
+
+## choose model
+
+
+# tsign:sdT:realm #########################
+# realm, no environmental temperature
+if (fitmod == 'modsdTRealmtsignAllJbeta') {
+    if (MATCHMOD)
+        stop('Model name matched more than one model!')
+    print(paste(sum(iallJbeta), 'data points'))
+    mod <- glmmTMB(
+        Jbeta.sc ~ duration +
+            REALM:tsign:tempchange_abs.sc:duration +
+            (duration | STUDY_ID / rarefyID),
+        data = trendsall[iallJbeta, ],
+        family = beta_family(link = 'logit'),
+        dispformula = ~ REALM
+    ) #,
+    #  control = glmmTMBControl(profile=TRUE)) # add dispersion formula
+    MATCHMOD <- TRUE
+}
+
+
+
+
+
+# tsign:rawT/T:sdT:REALM #########################
+if (fitmod == 'modrawTsdTTRealmtsignAllJbeta') {
+    if (MATCHMOD)
+        stop('Model name matched more than one model!')
+    print(paste(sum(iallJbeta), 'data points'))
+    mod <- glmmTMB(
+        Jbeta.sc ~ duration +
+            REALM:duration +
+            REALM:tsign:tempchange_abs.sc:duration +
+            REALM:tsign:tempave.sc:duration +
+            REALM:tsign:tempave.sc:tempchange_abs.sc:duration +
+            (duration | STUDY_ID / rarefyID),
+        data = trendsall[iallJbeta, ],
+        family = beta_family(link = 'logit'),
+        dispformula = ~ REALM
+    ) #,
+    #  control = glmmTMBControl(profile=TRUE)) # add dispersion formula
+    MATCHMOD <- TRUE
+}
+
+
+
+# print and save results ############################
+if (MATCHMOD == FALSE)
+    stop("Model name did not match anything", call. = FALSE)
+if (MATCHMOD) {
+    print(summary(mod))
+    saveRDS(mod, file = paste0('temp/', fitmod, '.rds'))
+    print(paste0('saved ', fitmod, '.rds'))
+    print(Sys.time())
+    print(warnings())
+    if (!grepl('dredge', fitmod)) {
+        print(performance::r2(mod)) # run if not a dredge object
+    }
+}
+
+print(warnings())
+print(paste('Ended', Sys.time(), sep = ''))
