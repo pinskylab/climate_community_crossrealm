2dff7cf0eb4b547178d5d6426d07280fdba552cd
Malin Pinsky <malin.pinsky@gmail.com>
2023-12-29 09:18:56 -0500

    Add a log-duration model

---
+++ code/turnover_GLMMlogduration_fit.R
@@ -0,0 +1,129 @@
+#!/usr/bin/Rscript --vanilla
+
+# Script to fit glmmTMB models with log(duration) terms
+# Set up to be run on the command line for one model at a time
+# Argument is model name to run (see below for options), e.g.
+# nohup code/turnover_GLMMlogduration_fit.R modLogDurInitAllJtu > logs/turnover_GLMMmodLogDurInitAllJtu.Rout &
+# (this works if code is executable, e.g., chmod u+x code/turnover_GLMMlogduration_fit.R)
+# (otherwise using nohup Rscript ...)
+
+# Read command line arguments ############
+
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+
+if (length(args) < 1)
+    stop("Have to specify a model to fit", call. = FALSE)
+if (length(args) > 1)
+    stop("Have to specify only 1 model to fit", call. = FALSE)
+fitmod <- args[1]
+MATCHMOD <-
+    FALSE # indicator to check if the argument matched a model name
+
+# print basic info about the job ############################
+
+print(paste('This is script turnover_GLMM_fit.R'))
+print(paste('This is process #', Sys.getpid()))
+print(Sys.time())
+
+
+
+# load libraries ############################
+
+library(data.table) # for handling large datasets
+library(glmmTMB, lib.loc = "/usr/lib64/R/library") # for ME models when loading on Annotate
+library(performance) # for R2
+
+# load data ############################
+
+# Turnover and covariates assembled by assemble_turnover_covariates.Rmd
+trendsall <- fread('output/turnover_w_covariates.csv.gz') # From assemble_turnover_covariates.Rmd
+
+trendsall[, tsign := as.factor(tsign)]
+trendsall[, duration.log := log(duration)]
+
+# calculate initial dissimilarities
+trendsall[, minduration := min(duration), by = rarefyID]
+initdiss <- trendsall[duration == minduration, .(Jtu.init = mean(Jtu.sc)), by = .(rarefyID)] # initial dissimilarities
+trendsall <- merge(trendsall, initdiss[, .(rarefyID, Jtu.init)])
+
+# Models ############################
+
+## Choose dataset
+iallJtu <-
+    trendsall[, complete.cases(
+        Jtu.sc,
+        tempchange_abs.sc,
+        REALM,
+        tempave.sc,
+        duration.sc,
+        microclim.sc,
+        human_bowler.sc
+    )]
+
+iallHorn <-
+    trendsall[, complete.cases(
+        Horn.sc,
+        tempchange_abs.sc,
+        REALM,
+        tempave.sc,
+        duration.sc,
+        microclim.sc,
+        human_bowler.sc
+    )]
+
+trendsall[iallJtu, absLat.sc := scale(abs(rarefyID_y))]
+
+## choose model
+
+# Baseline trend (null) model with log time term #################################
+if (fitmod == 'modLogDurInitAllJtu') {
+    if (MATCHMOD) stop('Model name matched more than one model!')
+    print(paste(sum(iallJtu), 'data points'))
+    print(paste(trendsall[iallJtu, length(unique(STUDY_ID))], 'studies'))
+    print(paste(trendsall[iallJtu, length(unique(rarefyID))], 'time series'))
+    mod <- glmmTMB(
+        Jtu.sc ~ duration.log +
+            Jtu.init:duration.log +
+            (duration | STUDY_ID / rarefyID),
+        data = trendsall[iallJtu, ],
+        family = beta_family(link = 'logit'),
+        dispformula = ~ REALM
+    )
+    MATCHMOD <- TRUE
+}
+
+if (fitmod == 'modLogDurInitAllHorn') {
+    if (MATCHMOD) stop('Model name matched more than one model!')
+    print(paste(sum(iallHorn), 'data points'))
+    print(paste(trendsall[iallHorn, length(unique(STUDY_ID))], 'studies'))
+    print(paste(trendsall[iallHorn, length(unique(rarefyID))], 'time series'))
+    mod <- glmmTMB(
+        Horn.sc ~ duration.log +
+            Jtu.init:duration.log +
+            (duration | STUDY_ID / rarefyID),
+        data = trendsall[iallHorn, ],
+        family = beta_family(link = 'logit'),
+        dispformula = ~ REALM,
+        control = glmmTMBControl(optimizer=optim, optArgs = list(method='BFGS'))
+    )
+    MATCHMOD <- TRUE
+}
+
+
+# print and save results ############################
+if (MATCHMOD == FALSE)
+    stop("Model name did not match anything", call. = FALSE)
+if (MATCHMOD) {
+    print(summary(mod))
+    saveRDS(mod, file = paste0('temp/', fitmod, '.rds'))
+    print(paste0('saved ', fitmod, '.rds'))
+    print(Sys.time())
+    print(warnings())
+    if (!grepl('dredge', fitmod)) {
+        print(performance::r2(mod)) # run if not a dredge object
+    }
+}
+
+print(warnings())
+print(paste('Ended', Sys.time(), sep = ''))
