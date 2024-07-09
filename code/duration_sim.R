bedc759413c821007455b6583b8f8a329054111f
Malin Pinsky <malin.pinsky@gmail.com>
2024-07-09 13:29:32 -0400

    Start to make sims more complex

--- code/duration_sim.R
+++ code/duration_sim.R
@@ -17,9 +17,6 @@ library(glmmTMB, lib.loc = '/usr/lib64/R/library/')
 
 
 # Functions ################
-# transformation for 2 categories. Eq. 1 in Douma & Weedon 2019 MEE
-transform01 <- function(x) (x * (length(x) - 1) + 0.5) / (length(x))
-
 slope <- function(x){
     mod <- lm(x ~ I(1:length(x)))
     return(as.numeric(coef(mod)[2]))
@@ -33,15 +30,14 @@ slopese <- function(x){
 
 # Simulations #############
 set.seed(5)
-nreps <- 100 # number of datasets
+nreps <- 100 # number of datasets at each duration range
+ndata <- 1000 # number timeseries per dataset
+nstudy <- 50 # number of studies per dataset. timeseries are nested within studies.
+studytable <- data.table(tsid = 1:ndata, studyid = rep(1:nstudy, ceiling(ndata/nstudy))[1:ndata]) # table to match study IDs to timeseries IDs
 
-cors <- data.table(n = 1000, minduration = rep(c(10,3,3,3,3), nreps), maxduration = rep(c(10, 12, 27, 52, 102), nreps), cor.p = NA_real_, cor.cor = NA_real_, lm.m = NA_real_,
+cors <- data.table(n = ndata, minduration = rep(c(10,3,3,3,3), nreps), maxduration = rep(c(10, 12, 27, 52, 102), nreps), cor.p = NA_real_, cor.cor = NA_real_, lm.m = NA_real_,
                    glmmwgt.p = NA_real_, glmmwgt.beta = NA_real_, glmmonegauss.p = NA_real_, glmmonegauss.beta = NA_real_,
-                   glmmonebeta.p = NA_real_, glmmonebeta.beta = NA_real_) # holds the summaries from each dataset. use n = 1000 timeseries per dataset.
-#cors2 <- copy(cors)
-#cors2[, n:=10000] # add another run with 10k timeseries per dataset
-#cors <- rbind(cors, cors2) 
-#rm(cors2)
+                   glmmonebeta.p = NA_real_, glmmonebeta.beta = NA_real_) # holds the summaries from each dataset.
 
 cors[, name := paste0(minduration, '-', maxduration)]
 cors[, range := maxduration - minduration]
@@ -51,25 +47,27 @@ print(paste0(nrow(cors), ' simulations to make'))
 for(i in 1:nrow(cors)){
     cat(i)
     
-    # make datasets with timeseries of all the same length
+    # prep to make datasets with timeseries of all the same length
     if(cors[i, minduration == maxduration]){
         len <- cors[i, minduration]
         dat <- data.table(tsid = rep(1:cors[i,n], rep(len, cors[i,n])))
+        dat <- merge(dat, studytable)
     }
     
-    # variable length timeseries
+    # prep to make datasets with variable length timeseries
     if(cors[i, minduration != maxduration]){
         mind <- cors[i, minduration]
         maxd <- cors[i, maxduration]
         ndur <- maxd - mind + 1
         dat <- data.table(tsid = rep(1:cors[i,n], rep(mind:maxd, cors[i,n]/ndur)))
+        dat <- merge(dat, studytable)
     }
     
     # add response and explanatory variables
-    dat[, ':='(obsid = 1:.N, tslope = runif(1, 0.005, 0.1)), by = tsid] # time counter and a random slope for each tsid
-    dat[, ':='(value1.logit = obsid*tslope + rnorm(.N), value2 = rnorm(.N))] # create random ts. value1 has a positive slope like dissimilarities and is on a logit scale.
+    dat[, ':='(obsid = 1:.N, tslope = runif(1, 0, 0.097)), by = tsid] # time counter and a random slope for each tsid
+    dat[, studyslope := runif(1, 0.01, 0.05), by = studyid] # a random slope for each studyid
+    dat[, ':='(value1.logit = obsid*studyslope + obsid*tslope + rnorm(.N), value2 = rnorm(.N))] # create two random timeseries. value1 has a positive slope like dissimilarities and is on a logit scale.
     dat[, value1 := exp(value1.logit)/(exp(value1.logit)+1)] # inverse logit-transform
-    dat[, value1.sc := transform01(value1)]
     dat[, duration := max(obsid), by = tsid]
     
     # calc slopes
@@ -77,6 +75,7 @@ for(i in 1:nrow(cors)){
     slopes <- dat[, .(slope1 = slope(value1), slope2_abs = unique(slope2_abs), se1 = slopese(value1)), by = tsid]
     
     # statistical tests
+    # NEED TO ADD STUDYID
     test <- slopes[, cor.test(slope1, slope2_abs)] # a pearson correlation
     test2 <- slopes[, coef(lm(slope1 ~ slope2_abs))[2]] # a simple regression
     test3 <- slopes[, summary(glmmTMB(slope1 ~ slope2_abs, disp=~se1))$coefficients$cond] # glmmTMB and dispersion: a meta-analysis model
